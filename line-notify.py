7e0aedf2499ee365f1a43fa73d94de2897141ac6
marn-pine <marn.pinescript@gmail.com>
2024-09-10 17:46:57 +0700

    10-sep-24

--- line-notify.py
+++ line-notify.py
@@ -1,11 +1,10 @@
 import requests
 
-token = 'your line token'
-
 def Notify(msg):
     url = 'https://notify-api.line.me/api/notify'
     header = {'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token}
     req = requests.post(url, headers=header, data={'message': msg})
     return(req)	
 
+token = 'your line token'
 print(Notify('your line message'))
